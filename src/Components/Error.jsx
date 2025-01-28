71734556c3ab0f1a059db0b683969733fabe3795
Abid0624 <abidkhan09989@gmail.com>
2025-01-28 22:36:50 +0600

    done

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,12 +1,8 @@
-import error from "../assets/assets/error.webp";
-
 const Error = () => {
   return (
     <div>
       <h2 className="text-3xl p-10 text-center">Page not found!</h2>
-      <div className="space-y-5 w-4/6 mx-auto">
-        <img src={error} alt="" />
-      </div>
+      <div className="space-y-5 w-4/6 mx-auto"></div>
     </div>
   );
 };
